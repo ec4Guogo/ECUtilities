fd612e63c77c30aaec275433a928ae0543b12184
ec4Guogo <guoming24@foxmail.com>
2015-06-14 22:16:37 +0800

    init demo project

---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by Eric Wang on 15/6/14.
+//  Copyright (c) 2015年 Eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
